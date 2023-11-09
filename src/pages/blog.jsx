32a971bedd91aab78858bfd7e8bcab728b77a58d
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-11-09 14:53:00 -0300

    fix: fix the thumbnails images for iOS an Safari

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -15,34 +15,44 @@ export function Blog() {
   const { allPosts } = data;
 
   return (
-    <div className="posts container">
-      {allPosts.slice(0, visiblePosts).map((post) => (
-        <Link to={`/blog/${post.slug}`} key={post.id}>
-          <div className="post-snippet">
-            <div className="thumbnail-container-blog">
+    <div className="posts-and-btn-container">
+      <div className="posts container">
+        {allPosts.slice(0, visiblePosts).map((post) => (
+          <Link to={`/blog/${post.slug}`} key={post.id}>
+            <div className="post-snippet">
+              {/* <div className="thumbnail-container-blog">
               <img
                 src={post.imagem.url}
                 alt={post.titulo}
                 className="thumbnail"
               />
-            </div>
-            <h1>{post.titulo}</h1>
-            <p className="post-date">{post.data}</p>
-            <div className="avatar-name-container">
+            </div> */}
               <img
-                src={post.autor.avatar.url}
-                width={90}
-                alt="avatar"
-                className="avatar"
+                src={post.imagem.url}
+                alt={post.titulo}
+                className="thumbnail"
               />
-              {/* <p className="author-name fw-700">{post.autor.nome}</p> */}
-              <p className="author-name">{post.autor.nome}</p>
+              <h1>{post.titulo}</h1>
+              <p className="post-date">{post.data}</p>
+              <div className="avatar-name-container">
+                <img
+                  src={post.autor.avatar.url}
+                  width={90}
+                  alt="avatar"
+                  className="avatar"
+                />
+                {/* <p className="author-name fw-700">{post.autor.nome}</p> */}
+                <p className="author-name">{post.autor.nome}</p>
+              </div>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
+      </div>
       {visiblePosts < allPosts.length && (
-        <button onClick={() => setVisiblePosts(visiblePosts + 2)} className="load-more-btn">
+        <button
+          onClick={() => setVisiblePosts(visiblePosts + 2)}
+          className="load-more-btn"
+        >
           Carregar mais posts
         </button>
       )}
